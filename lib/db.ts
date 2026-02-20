fb83d1d4e358553bc4113d433db1981b20920a73
Natalie <natalie.ta@lumenalta.com>
2026-02-19 19:39:35 -0500

    fixed error and removed login

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,6 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL ?? "";
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
